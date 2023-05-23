c9ef9948a90c3b1181cce2b45a79bab1d2ffb2fa
helinatefera <helinatefera1212@gmail.com>
2023-05-23 00:55:21 -0700

    0

--- 0x0E./dog.h
+++ 0x0E./dog.h
@@ -1,14 +1,14 @@
-#ifndef _HEADER_
-#define _HEADER_
+#ifndef DOG_H
+#define DOG_H
 
 /**
-  * struct dog - Dog attributes
-  * @name: The name of the dog.
-  * @age: The age of the dog.
-  * @owner: The owner of the dog.
-  *
-  * Description: The attributes of a dog.
-  */
+ * struct dog - a dog's basic info
+ * @name: First member
+ * @age: Second member
+ * @owner: Third member
+ *
+ * Description: Longer description
+ */
 struct dog
 {
 	char *name;
@@ -17,7 +17,7 @@ struct dog
 };
 
 /**
- * dog_t - Typedef for dog structure
+ * dog_t - typedef for struct dog
  */
 typedef struct dog dog_t;
 
@@ -25,4 +25,7 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+char *_strcpy(char *dest, char *src);
+int _strlen(char *s);
+
 #endif
